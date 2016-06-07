b27026d034fc7b62ead35595cf63b636a97ae5b9
Baptiste Jamin <baptistejamin@gmail.com>
2016-06-07 19:33:39 +0200

    gemfile

---
+++ ruby-crisp-api.rb
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'ruby-crisp-api'
+  s.version     = '1.0.0'
+  s.date        = '-06-07'
+  s.summary     = "Crisp Ruby API"
+  s.description = "Crisp Ruby API"
+  s.authors     = ["Baptiste Jamin"]
+  s.email       = 'baptiste@crisp.im'
+  s.files       = ["lib/crisp.rb"]
+  s.homepage    = 'https://crisp.im'
+  s.license     = 'MIT'
+end
