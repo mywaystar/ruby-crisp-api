9c607c4237d7414222a7325fde776151592acfd2
Baptiste Jamin <baptistejamin@gmail.com>
2016-06-07 19:47:27 +0200

    fix gemspec

--- ruby-crisp-api.rb
+++ ruby-crisp-api.rb
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'ruby-crisp-api'
-  s.version     = '1.0.0'
+  s.version     = '1.0.1'
   s.date        = Date.today
   s.summary     = "Crisp Ruby API"
   s.description = "Crisp Ruby API"
@@ -13,6 +13,6 @@ Gem::Specification.new do |s|
   s.require_paths         = ['lib']
   s.required_ruby_version = '>= 2.2'
   s.add_dependency 'rest-client', '~> 1.8.0'
-  s.add_dependency 'socket.io-client-simple', '~> 2.1.0'
+  s.add_dependency 'socket.io-client-simple', '~> 1.2.0'
   s.add_dependency 'event_emitter', '~> 0.2.4'
 end
